14f533081a5127be805976df3e3cd15f6ea2ff22
BhigKhali <123639682+BhigKhali@users.noreply.github.com>
2024-12-08 22:37:23 +0000

    Leaderboard
    Mock LeaderBoard page

--- src/app/score/score.page.ts
+++ src/app/score/score.page.ts
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./score.page.scss'],
 })
 export class ScorePage implements OnInit {
+  players: { name: string; score: number }[] = [];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
-  }
+    // test player data for now
+    this.players = [
+      { name: 'Khali', score: 10 },
+      { name: 'Sean', score: 110 },
+      { name: 'Ayodeji', score: 15 },
+      { name: 'Ryan', score: 85 },
+      { name: 'Diddy', score: 69 },
+    ];
 
+
+    // sort players by score (high to  lowe score)
+  this.players.sort((a, b) => b.score - a.score);
+  }
 }
