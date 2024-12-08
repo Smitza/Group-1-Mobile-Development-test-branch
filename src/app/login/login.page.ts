d378bf148d5c8771ed8bfef5d628a7a89987eb2f
Smitza <seansj02@gmail.com>
2024-12-08 19:19:27 +0000

    Login data passing and front-end design

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
+export class LoginPage {
+  email: string = '';
+  password: string = '';
+  emailError: boolean = false;
+  passwordError: boolean = false;
 
-  constructor() { }
+  constructor(private router: Router) {}
 
-  ngOnInit() {
+  validateAndLogin() {
+    this.emailError = false;
+    this.passwordError = false;
+
+    // Email validation regex
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    if (!emailPattern.test(this.email)) {
+      this.emailError = true;
+    }
+
+    if (this.password.length < 6) {
+      this.passwordError = true;
+    }
+
+    if (!this.emailError && !this.passwordError) {
+      this.router.navigate(['/tabs/profile'], { queryParams: { email: this.email } });
+    }
   }
 
+  goToProfile() {
+    this.router.navigate(['/tabs/profile'], { queryParams: { email: this.email } });
+  }
 }
