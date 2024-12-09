50ec7f651c94d357c06977c2bdbfe728d3a48ae3
Destiny <146720629+FVGhost@users.noreply.github.com>
2024-12-09 15:45:31 +0000

    finished profle

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -8,8 +8,32 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ProfilePage implements OnInit {
   email: string = '';
+  profileImage: string = ''; 
+  currentScore: number = 0;
+  lastGamePlayed: string = ''; 
 
-  constructor(private route: ActivatedRoute) {}
+
+  private imageList: string[] = [
+    'assets/images/profile1.png',
+    'assets/images/profile2.png',
+    'assets/images/profile3.png',
+    'assets/images/profile4.png',
+    'assets/images/profile5.png',
+    'assets/images/profile6.png',
+    'assets/images/profile7.png',
+    'assets/images/profile8.png',
+    'assets/images/profile9.png',
+    'assets/images/profile10.png',
+
+  ];
+
+  private gamesList: string[] = [
+    'Easy',
+    'Medium',
+    'Hard'
+  ];
+
+  constructor(private route: ActivatedRoute, private router: Router) {}
 
   ngOnInit() {
     // Retrieve the email from the query parameters
@@ -18,5 +42,32 @@ export class ProfilePage implements OnInit {
         this.email = params['email'];
       }
     });
+
+    // Randomly select an image, current score, and last game played
+    this.randomizeProfileDetails();
+  }
+
+  randomizeProfileDetails() {
+    // Randomize the profile image
+    const randomImageIndex = Math.floor(Math.random() * this.imageList.length);
+    this.profileImage = this.imageList[randomImageIndex];
+
+    // Randomize the current score between 0 and 1000
+    this.currentScore = Math.floor(Math.random() * 1001);
+
+    // Randomly select the last game played
+    const randomGameIndex = Math.floor(Math.random() * this.gamesList.length);
+    this.lastGamePlayed = this.gamesList[randomGameIndex];
+  }
+
+  editProfile() {
+    console.log('Edit profile clicked');
+    // Add profile editing logic here
+  }
+
+  logout() {
+    console.log('Logout clicked');
+    // Clear session data and navigate to the login page
+    this.router.navigate(['/login']);
   }
 }
